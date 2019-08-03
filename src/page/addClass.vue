dc9cc97b3c971ee35d58b87fa8c8d42990fa6d39
cqq <caiqingqiu@bonc.com.cn>
2019-08-03 21:24:18 +0800

    样式

--- src/page/addClass.vue
+++ src/page/addClass.vue
@@ -3,46 +3,73 @@
         <SideNav></SideNav>
         <div class='right-container'>
             <Header class="header" :headerList="headerList"></Header>
-            <div class="right-content">
-                <el-form ref="form" :model="form" label-width="80px">
-                    <el-form-item label="班级名称">
-                        <el-input v-model="form.name"></el-input>
-                    </el-form-item>
-                    <el-form-item label="学科">
-                        <el-select v-model="form.region" placeholder="请选择活动区域">
-                            <el-option label="区域一" value="shanghai"></el-option>
-                            <el-option label="区域二" value="beijing"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="讲师">
-                        <el-col :span="11">
-                            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;">
-                            </el-date-picker>
-                        </el-col>
-                        <el-col class="line" :span="2">-</el-col>
-                        <el-col :span="11">
-                            <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;">
-                            </el-time-picker>
-                        </el-col>
-                    </el-form-item>
-                    <el-form-item label="助教">
-                        <el-switch v-model="form.delivery"></el-switch>
-                    </el-form-item>
-                    <el-form-item label="开课时间">
-                        <el-checkbox-group v-model="form.type">
-                            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                            <el-checkbox label="地推活动" name="type"></el-checkbox>
-                            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                        </el-checkbox-group>
-                    </el-form-item>
-                    <el-form-item label="结束时间">
-                        <el-radio-group v-model="form.resource">
-                            <el-radio label="线上品牌商赞助"></el-radio>
-                            <el-radio label="线下场地免费"></el-radio>
-                        </el-radio-group>
-                    </el-form-item>
-                </el-form>
+            <div class="right-content white-color">
+                <div class="add-title">
+                    <img src="../../src/assets/img/welcome/add.png" alt="">
+                    <span>新增班级</span>
+                </div>
+                <div class="add-form-container">
+                    <el-form ref="form" :model="form" label-width="80px" class="add-form">
+                        <el-form-item label="班级名称">
+                            <el-input v-model="form.name" placeholder="请填写班级名称"></el-input>
+                        </el-form-item>
+                        <el-form-item label="学科">
+                            <el-select v-model="form.region" placeholder="请选择">
+                                <el-option label="数学" value="shanghai"></el-option>
+                                <el-option label="语文" value="beijing"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="讲师">
+                            <el-col>
+                                <el-select v-model="form.region" placeholder="请选择">
+                                    <el-option label="小名" value="shanghai"></el-option>
+                                    <el-option label="小王" value="beijing"></el-option>
+                                </el-select>
+                            </el-col>
+                        </el-form-item>
+                        <el-form-item label="助教">
+                            <el-select v-model="form.region" placeholder="请选择">
+                                <el-option label="小名" value="shanghai"></el-option>
+                                <el-option label="小王" value="beijing"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="开课时间">
+                            <el-col :span="11">
+                                <el-form-item prop="date1">
+                                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1"
+                                        style="width: 100%;"></el-date-picker>
+                                </el-form-item>
+                            </el-col>
+                            <el-col class="line date-line" :span="2">-</el-col>
+                            <el-col :span="11">
+                                <el-form-item prop="date2">
+                                    <el-time-picker placeholder="选择时间" style="width: 100%;">
+                                    </el-time-picker>
+                                </el-form-item>
+                            </el-col>
+                        </el-form-item>
+                        <el-form-item label="结束时间">
+                            <el-col :span="11">
+                                <el-form-item prop="date1">
+                                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1"
+                                        style="width: 100%;"></el-date-picker>
+                                </el-form-item>
+                            </el-col>
+                            <el-col class="line date-line" :span="2">-</el-col>
+                            <el-col :span="11">
+                                <el-form-item prop="date2">
+                                    <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;">
+                                    </el-time-picker>
+                                </el-form-item>
+                            </el-col>
+                        </el-form-item>
+                        <el-form-item class="btn-group">
+                            <el-button type="primary">确定</el-button>
+                            <el-button>重置</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+
             </div>
         </div>
     </div>
@@ -57,7 +84,7 @@
         name: 'home',
         data() {
             return {
-                headerList: ['会员', '欢迎', '欢迎页'],
+                headerList: ['会员', '班级', '新增班级'],
                 form: {
                     name: '',
                     region: '',
@@ -96,12 +123,51 @@
         height: 100%
     }
 
+    .add-title {
+        height: 70px;
+        line-height: 70px;
+        padding-left: 30px;
+
+        img {
+            height: 20px;
+            width: 20px;
+            position: relative;
+            top: 4px;
+        }
+    }
+
+    .add-form-container {
+        width: calc(100% - 60px);
+        margin: auto;
+        border: 1px solid rgba(221, 223, 230, 1);
+
+        .add-form {
+            width: 500px;
+            margin: auto;
+            margin-top: 30px;
+        }
+
+        .el-select {
+            width: 100%
+        }
+    }
+
 </style>
 <style lang="less">
     .left-container {
         ul {
             background-color: #22345E
         }
+    }
+
+    .btn-group {
+        >div {
+            text-align: center
+        }
+    }
+
+    .date-line {
+        text-align: center !important
     }
 
 </style>
