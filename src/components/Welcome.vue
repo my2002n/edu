4410ac7aa3585e5620b3c08f1d119fbd2e5d6990
cqq <caiqingqiu@bonc.com.cn>
2019-08-03 16:23:13 +0800

    图片

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,64 +2,64 @@
     <div class="welcome-container">
         <!-- 第一行 -->
         <div class="row1">
-            <div>
+            <div class="row11">
                 <div class="title">
                     有效期: 2019-10-12
                 </div>
                 <div class="img test">
-                    <img src="../../src/assets/img/welcome/orangePlus.png" alt="">
+                    <!-- <img src="../../src/assets/img/welcome/orangePlus.png" alt=""> -->
                 </div>
                 <div class="plus-btn btn">Plus会员费</div>
                 <div class="ins">（¥298/年，¥498/3年，享受本系）</div>
             </div>
-            <div>
+            <div class="row12">
                 <div class="title">
                     未配置
                 </div>
                 <div class="img">
-                    <img src="../../src/assets/img/welcome/blueIe.png" alt="">
+                    <!-- <img src="../../src/assets/img/welcome/blueIe.png" alt=""> -->
                 </div>
                 <div class="blue-btn btn">配置独立域名，品牌</div>
                 <div class="ins">（¥988一次性实施费，无需每年续费）</div>
             </div>
-            <div>
+            <div class="row13">
                 <div class="title">
                     同时直播数：30并发
                 </div>
                 <div class="img">
-                    <img src="../../src/assets/img/welcome/greenClass.png" alt="">
+                    <!-- <img src="../../src/assets/img/welcome/greenClass.png" alt=""> -->
                 </div>
                 <div class="green-btn btn">大班课直播</div>
                 <div class="ins">（¥35/月/并发，10并发起售。讲师直播为主学生可举手发言）</div>
             </div>
         </div>
         <div class="row2">
-            <div>
+            <div class="row21">
                 <div class="title">
                     剩余：89088播点
                 </div>
                 <div class="img">
-                    <img src="../../src/assets/img/welcome/redShow.png" alt="">
+                    <!-- <img src="../../src/assets/img/welcome/redShow.png" alt=""> -->
                 </div>
                 <div class="red-btn btn">小班课1v1直播</div>
                 <div class="ins">（¥10/100播点，5播点/师生/小时。全视频直播授课）</div>
             </div>
-            <div>
+            <div class="row22">
                 <div class="title">
                     剩余：98/1000G
                 </div>
                 <div class="img">
-                    <img src="../../src/assets/img/welcome/orangeRedis.png" alt="">
+                    <!-- <img src="../../src/assets/img/welcome/orangeRedis.png" alt=""> -->
                 </div>
                 <div class="plus-btn btn">点播缓存</div>
                 <div class="ins">（¥10/20G/月，直播回放，点播课程存储空间）</div>
             </div>
-            <div>
+            <div class="row23">
                 <div class="title">
                     剩余100G
                 </div>
                 <div class="img">
-                    <img src="../../src/assets/img/welcome/blueFlow.png" alt="">
+                    <!-- <img src="../../src/assets/img/welcome/blueFlow.png" alt=""> -->
                 </div>
                 <div class="blue-btn btn">点播流量</div>
                 <div class="ins">（¥10/50G流量，直播回放，点播课程播放流量费含CDN费用）</div>
@@ -88,9 +88,9 @@
     @import url('../../src/assets/less/Mixins.less');
 
     .welcome-container {
-        .test {
-            /*background:  url('../../src/assets/img/welcome/orangePlus.png')  center no-repeat;*/
-        }
+        // .test {
+        //     background: url('../../src/assets/img/welcome/orangePlus.png') center no-repeat;
+        // }
 
         .row1,
         .row2 {
@@ -104,9 +104,34 @@
                 margin-top: 20px;
                 height: 310px;
 
+
                 &:last-child {
                     margin-right: 0px;
                 }
+            }
+
+            .row11 {
+                background: url('../../src/assets/img/welcome/orangePlus.png') center no-repeat;
+            }
+
+            .row12 {
+                background: url('../../src/assets/img/welcome/blueIe.png') center no-repeat;
+            }
+
+            .row13 {
+                background: url('../../src/assets/img/welcome/greenClass.png') center no-repeat;
+            }
+
+            .row21 {
+                background: url('../../src/assets/img/welcome/redShow.png') center no-repeat;
+            }
+
+            .row22 {
+                background: url('../../src/assets/img/welcome/orangeRedis.png') center no-repeat;
+            }
+
+            .row23 {
+                background: url('../../src/assets/img/welcome/blueFlow.png') center no-repeat;
             }
 
             .ins {
@@ -125,11 +150,11 @@
         .img {
             text-align: center;
 
-            img {
-                height: 140px;
-            }
+            // img {
+            //     height: 140px;
+            // }
 
-            /*height: 140px;*/
+            height: 140px;
             /*background-size: 100%;*/
 
         }
