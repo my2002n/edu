51d9cb8334015f040db068cb2e2384565ecc7bc5
cqq <caiqingqiu@bonc.com.cn>
2019-08-07 13:10:46 +0800

    页面

--- src/page/teacherList.vue
+++ src/page/teacherList.vue
@@ -4,7 +4,65 @@
         <div class='right-container'>
             <Header class="header" :headerList="headerList"></Header>
             <div class="right-content white-color">
-                敬请期待
+                <div class="search-contain">
+                    <el-form :inline="true" :model="form" class="demo-form-inline">
+                        <el-form-item label="账号">
+                            <el-input v-model="form.user" placeholder="账号" class="static-input">
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item label="姓名">
+                            <el-input v-model="form.user" placeholder="姓名" class="static-input">
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item label="手机号码">
+                            <el-input v-model="form.user" placeholder="手机号码" class="static-input">
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item label="状态">
+                            <el-input v-model="form.user" placeholder="状态" class="static-input">
+                            </el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary">查询</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+                <div class="content-contain">
+                    <el-table :data="tableData" style="width: 100%"
+                        :header-cell-style="{background:'#EFF1F6',color:'#1C1C1C'}" stripe>
+                        <el-table-column label="账号" prop="id">
+                        </el-table-column>
+                        <el-table-column label="姓名" prop="name">
+                        </el-table-column>
+                        <el-table-column label="邀请码" prop="name">
+                        </el-table-column>
+                        <el-table-column label="手机号码" prop="name">
+                        </el-table-column>
+                        <el-table-column label="身份" prop="name">
+                        </el-table-column>
+                        <el-table-column label="查看课程" prop="name">
+                        </el-table-column>
+                        <el-table-column label="状态" prop="name">
+                        </el-table-column>
+                        <el-table-column label="操作">
+                            <template slot-scope="scope">
+                                <span class="operate">
+                                    <img src="../../src/assets/img/welcome/edit.png" alt="">
+                                </span>
+                                <span class="operate">
+                                    <img src="../../src/assets/img/welcome/add.png" alt="">
+                                </span>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </div>
+                <div class="footer-contain">
+                    <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                        :page-sizes="[100, 200, 300, 400]" :page-size="100"
+                        layout="total, sizes, prev, pager, next, jumper" :total="400">
+                    </el-pagination>
+                    <div class='go-page'>确定</div>
+                </div>
             </div>
         </div>
     </div>
@@ -30,7 +88,32 @@
                     type: [],
                     resource: '',
                     desc: ''
-                }
+                },
+                tableData: [{
+                    id: '高三理科综合',
+                    name: '这是今天报名的学生',
+                    category: '江浙小吃、小吃零食',
+                    desc: '荷兰优质淡奶，奶香浓而不腻',
+                    address: '上海市普陀区真北路',
+                    shop: '王小虎夫妻店',
+                    shopId: '10333'
+                }, {
+                    id: '高三理科综合',
+                    name: '这是今天报名的学生',
+                    category: '江浙小吃、小吃零食',
+                    desc: '荷兰优质淡奶，奶香浓而不腻',
+                    address: '上海市普陀区真北路',
+                    shop: '王小虎夫妻店',
+                    shopId: '10333'
+                }, {
+                    id: '高三理科综合',
+                    name: '这是今天报名的学生',
+                    category: '江浙小吃、小吃零食',
+                    desc: '荷兰优质淡奶，奶香浓而不腻',
+                    address: '上海市普陀区真北路',
+                    shop: '王小虎夫妻店',
+                    shopId: '10333'
+                }],
             }
         },
         methods: {
@@ -59,51 +142,84 @@
         height: 100%
     }
 
-    .add-title {
-        height: 70px;
-        line-height: 70px;
-        padding-left: 30px;
+    .search-contain {
+        // margin-top: 20px;
+        padding-left: 20px;
+        padding-top: 24px;
+    }
+
+    .content-contain {
+        // padding-left: 16px;
+    }
+
+    .right-container {
+        background: #ffffff;
+        height: 100%;
+    }
+
+    .operate {
+        cursor: pointer;
+        margin-right: 20px;
 
         img {
-            height: 20px;
-            width: 20px;
-            position: relative;
-            top: 4px;
+            width: 16px;
+            height: 16px;
         }
     }
 
-    .add-form-container {
-        width: calc(100% - 60px);
-        margin: auto;
-        border: 1px solid rgba(221, 223, 230, 1);
-
-        .add-form {
-            width: 500px;
-            margin: auto;
-            margin-top: 30px;
-        }
-
-        .el-select {
-            width: 100%
+    .grid-content {
+        span {
+            position: relative;
+            margin-right: 5px;
+            top: 3px;
         }
     }
 
 </style>
 <style lang="less">
-    .left-container {
-        ul {
-            background-color: #22345E
+    .el-table {
+        border: 1px solid rgba(223, 230, 236, 1) !important;
+    }
+
+    .el-table__header-wrapper {
+        height: 40px !important;
+        background: rgba(239, 241, 246, 1) !important;
+    }
+
+    .el-table__header tr,
+    .el-table__header th {
+        padding: 0 !important;
+        height: 40px !important;
+    }
+
+    .el-table__body-wrapper {
+        .el-row {
+            margin-bottom: 16px !important;
+
+            &:first-child {
+                margin-top: 10px !important;
+            }
+        }
+
+        tbody {
+
+            .table__row,
+            td {
+                padding: 8px 0px !important;
+
+            }
+
         }
     }
 
-    .btn-group {
-        >div {
-            text-align: center
-        }
+
+    .static-input {
+        width: 150px !important;
+        height: 40px !important;
     }
 
-    .date-line {
-        text-align: center !important
+    .el-form-item__content {
+        margin-right: 20px
     }
 
 </style>
