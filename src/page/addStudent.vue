8a786db449b920fc882bbab53e8d83beca211132
cqq <caiqingqiu@bonc.com.cn>
2019-08-07 12:26:50 +0800

    新增学生

--- src/page/addStudent.vue
+++ src/page/addStudent.vue
@@ -4,7 +4,38 @@
         <div class='right-container'>
             <Header class="header" :headerList="headerList"></Header>
             <div class="right-content white-color">
-                敬请期待
+                <div class="add-title">
+                    <img src="../../src/assets/img/welcome/add.png" alt="">
+                    <span>新增学生</span>
+                </div>
+                <div class="add-form-container">
+                    <el-form ref="form" :model="form" label-width="80px" class="add-form">
+                        <el-form-item label="手机号码">
+                            <el-input v-model="form.phone"></el-input>
+                        </el-form-item>
+                        <el-form-item label="学生姓名">
+                            <el-input v-model="form.name" placeholder="请输入学生姓名"></el-input>
+                        </el-form-item>
+                        <el-form-item label="出生日期">
+                            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;">
+                            </el-date-picker>
+                        </el-form-item>
+                        <el-form-item label="性别">
+                            <el-select v-model="form.region" placeholder="请选择">
+                                <el-option label="男" value="shanghai"></el-option>
+                                <el-option label="女" value="beijing"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="备注">
+                            <el-input type="textarea" v-model="form.desc"></el-input>
+                        </el-form-item>
+                        <el-form-item class="btn-group">
+                            <el-button type="primary">保存</el-button>
+                            <el-button>取消</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+
             </div>
         </div>
     </div>
@@ -23,6 +54,7 @@
                 openeds: ['2', '2-1'],
                 form: {
                     name: '',
+                    phone: '15694267318',
                     region: '',
                     date1: '',
                     date2: '',
